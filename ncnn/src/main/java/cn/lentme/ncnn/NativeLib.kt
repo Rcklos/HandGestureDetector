749619cc19cff52b3773e2f93baebfdc722904a6
LOS <2362819691@qq.com>
2022-12-07 13:34:22 +0800

    两个ncnn会使OpenMP重复init导致abort

---
+++ ncnn/src/main/java/cn/lentme/ncnn/NativeLib.kt
@@ -0,0 +1,17 @@
+package cn.lentme.ncnn
+
+class NativeLib {
+
+    /**
+     * A native method that is implemented by the 'ncnn' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+        // Used to load the 'ncnn' library on application startup.
+        init {
+            System.loadLibrary("ncnn")
+        }
+    }
+}
